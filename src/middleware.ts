9ba8d72b01abd53e2c5022c78c63ba522d19087b
zombie <onetouchX9@gmail.com>
2024-04-23 15:33:38 +0500

    initial setup

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: [
+    "/((?!.*\\..*|_next).*)", // Don't run middleware on static files
+    "/", // Run middleware on index page
+    "/(api|trpc)(.*)",
+  ], // Run middleware on API routes
+};
